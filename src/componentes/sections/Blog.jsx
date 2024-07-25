84d1766bb12a80d0ef3bf7e743aba4ef5ddbaf67
Deyson Santana <deyson.s.jesus@hotmail.com>
2024-07-25 09:58:25 -0300

    Versao 1.1
    aaaaa

--- src/componentes/sections/Blog.jsx
+++ src/componentes/sections/Blog.jsx
@@ -10,13 +10,13 @@ function Blog(){
             <Cards
                 title="Além do Básico: Explorando Recursos Avançados na Tecnologia para Aprimorar seu Site"
                 date="11 Dez 2023"
-                tags="site, dicas, tec"
+                tags="site, dicas, tech"
                 description="A evolução constante da tecnologia digital oferece uma infinidade de recursos para aprimorar seu site, transformando-o em uma ferramenta mais..."
             />
             <Cards
                 title="O que é preciso para ter um site?"
                 date="17 Dez 2023"
-                tags="site, dicas, tec"
+                tags="site, dicas, tech"
                 description="Então, chegou o momento e você está decidido a trazer sua empresa para o mundo digital! Parabéns, ficamos felizes por ter tomado essa decisão. Mas tenho que perguntar: porque demorou tanto?"
             />
 
